76f48cc0329bb9fa7ac57702823213e07962b63f
gimble911 <2005295@laurea.fi>
2021-12-04 15:10:42 +0200

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'newtask',
+    loadChildren: () => import('./newtask/newtask.module').then( m => m.NewtaskPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
+import { NewtaskPage } from './../newtask/newtask.page';
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+todoList = []
+
+  today : number = Date.now()
+
+  constructor(public modalCtrl:ModalController) {}
+
+  async addTask(){
+    const modal = await this.modalCtrl.create({
+      component: NewtaskPage
+    })
+
+    modal.onDidDismiss().then(newTaskObj => {
+      console.log(newTaskObj.data);
+      this.todoList.push(newTaskObj.data)
+    })
+
+    return await modal.present()
+
+  }
+
+  delete(index){
+    this.todoList.splice(index,1)
+
+  }
 
 }
